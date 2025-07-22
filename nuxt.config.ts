3dfab49e9879bdb8d7fc1b59701d9cc51be070d2
Gray <digrayman8@gmail.com>
2025-07-22 09:10:13 +0900

    some fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: [
-    '../assets/global.css'
+    '~/assets/global.css'
   ],
   ssr: false, // Disable Server Side rendering
   compatibilityDate: '2025-05-15',
